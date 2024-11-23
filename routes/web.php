2e17adabe35f922860eeeabe8f82c2a96f6e5e9b
Irfan Maulana Khakiki <irfankhakiki17@gmail.com>
2024-11-23 13:57:14 +0700

    Membuat tabel customer, model, controller, dan route resource untuk crm-user, unit-kerja, dan response

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CrmUserController;
+use App\Http\Controllers\UnitKerjaController;
+use App\Http\Controllers\ResponseController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('crm-users', CrmUserController::class);
+Route::resource('unit-kerja', UnitKerjaController::class);
+Route::resource('responses', ResponseController::class);
+
