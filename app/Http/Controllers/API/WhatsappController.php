1242a0207968ab80ac57a609ec986b9c65324aa6
Nafi'ul Anam <anamnafiul99@gmail.com>
2024-12-19 17:23:14 +0700

    move WhatsappController to API

--- app/Http/Controllers/API/WhatsappController.php
+++ app/Http/Controllers/API/WhatsappController.php
@@ -3,11 +3,36 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Services\WhatsappService;
 
-class WhatsappController extends Controller
+class WhatsAppController extends Controller
 {
-    public function createInstance() {}
-    public function sendMessage($number, $message, $instance) {}
-    public function setWebhook() {}
-    public function sendMedia($number, $message, $instance, $media) {}
+    protected $whatsAppService;
+
+    public function __construct(WhatsappService $whatsAppService)
+    {
+        $this->whatsAppService = $whatsAppService;
+    }
+
+    public function send(Request $request)
+    {
+        $request->validate([
+            'number' => 'required',
+            'type' => 'required',
+            'message' => 'required',
+            'instance_id' => 'required',
+            'access_token' => 'required',
+        ]);
+
+        $number = $request->input('number');
+        $type = $request->input('type');
+        $message = $request->input('message');
+        $instance_id = $request->input('instance_id');
+        $token = $request->input('access_token');
+
+        $response = $this->whatsAppService->sendMessage($number, $type, $message, $instance_id, $token);
+
+        return response()->json($response);
+    }
 }
