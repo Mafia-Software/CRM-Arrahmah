ef691fa42bca288fe97ea1af2cbcf2d53c3f57d9
irfanmkh <irfankhakiki17@gmail.com>
2024-12-19 21:29:30 +0700

    update route ke controller

--- routes/web.php
+++ routes/web.php
@@ -3,19 +3,10 @@
 use App\Services\WhatsappService;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\WhatsappController;
 
 Route::get('/', function () {
     return redirect('/admin');
 });
 
-Route::get('/test-whatsapp', function (WhatsappService $whatsAppService) {
-    $number = request('number'); // Nomor penerima dari Postman
-    $type = request('type'); // Tipe pesan dari Postman
-    $message = request('message'); // Isi pesan dari Postman
-    $instance_id = request('instance_id'); // Instance ID dari Postman
-    $token = request('access_token'); // Token akses dari Postman
-
-    $response = $whatsAppService->sendMessage($number, $message, $type, $instance_id, $token);
-
-    return response()->json($response);
-});
+Route::get('/test-whatsapp', [WhatsappController::class, 'send']);
