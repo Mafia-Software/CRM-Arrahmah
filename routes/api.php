c7546025cb34d17dee26ec92faa2a1defb572943
Nafi'ul Anam <anamnafiul99@gmail.com>
2024-12-23 08:49:17 +0700

    add api route

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
 Route::prefix('wa')->group(function () {
     Route::post('/sendMessage', [App\Http\Controllers\API\WhatsappController::class, 'send']);
 });
