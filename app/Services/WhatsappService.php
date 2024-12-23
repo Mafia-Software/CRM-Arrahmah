8d2075c86942e5303668a2de05c48433844abaa2
Nafi'ul Anam <anamnafiul99@gmail.com>
2024-12-23 08:48:57 +0700

    add getQR to whatsapp service

--- app/Services/WhatsappService.php
+++ app/Services/WhatsappService.php
@@ -30,6 +30,28 @@ class WhatsappService
             'access_token' => $this->accessToken
         ]);
 
+        // Memeriksa status dan respons
+        if ($response->successful()) {
+            // Jika berhasil, mengembalikan respons JSON
+            return $response->json();
+        } else {
+            // Jika gagal, mengembalikan status dan pesan error
+            return [
+                'status' => $response->status(),
+                'error' => $response->body()
+            ];
+        }
+    }
+    public function getQR($instance_id)
+    {
+        $url = 'https://new.sentwa.com/api/get_qr.php';
+
+        $response = Http::withOptions([
+            "verify" => false,
+        ])->get($url, [
+            'instance_id' => $instance_id,
+            'access_token' => $this->accessToken
+        ]);
 
         // Memeriksa status dan respons
         if ($response->successful()) {
