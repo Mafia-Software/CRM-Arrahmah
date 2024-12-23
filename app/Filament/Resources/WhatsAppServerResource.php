c6de543744f363cffe1dc71b64e08a378c259ece
Nafi'ul Anam <anamnafiul99@gmail.com>
2024-12-23 08:48:39 +0700

    remove instance_id from form, add column and custom action

--- app/Filament/Resources/WhatsAppServerResource.php
+++ app/Filament/Resources/WhatsAppServerResource.php
@@ -3,32 +3,35 @@
 namespace App\Filament\Resources;
 
 use App\Filament\Resources\WhatsAppServerResource\Pages;
-use App\Filament\Resources\WhatsAppServerResource\RelationManagers;
-use App\Models\WhatsAppServer;
-use Filament\Forms;
+use App\Models\WhatsappServer;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Actions\Action;
+use Filament\Tables\Actions\ActionGroup;
+use Filament\Tables\Actions\DeleteAction;
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
+
 
 class WhatsAppServerResource extends Resource
 {
-    protected static ?string $model = WhatsAppServer::class;
-
+    protected static ?string $model = WhatsappServer::class;
+    protected static ?string $navigationLabel = "Whatsapp Server";
+    protected static ?int $navigationSort = 2;
     protected static ?string $navigationIcon = 'heroicon-o-server-stack';
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-                Forms\Components\TextInput::make('nama')
+                TextInput::make('nama')
                     ->required()
                     ->maxLength(255),
-                Forms\Components\TextInput::make('instance_id')
-                    ->required()
-                    ->maxLength(255),
+                TextInput::make('no_wa')->label('No. Whatsapp')->required()->numeric(),
             ]);
     }
 
@@ -36,15 +39,18 @@ class WhatsAppServerResource extends Resource
     {
         return $table
             ->columns([
-                Tables\Columns\TextColumn::make('nama')
+                TextColumn::make('nama')
                     ->searchable(),
-                Tables\Columns\TextColumn::make('instance_id')
+                TextColumn::make('no_wa')
                     ->searchable(),
-                Tables\Columns\TextColumn::make('created_at')
+                TextColumn::make('instance_id')
+                    ->searchable()->label('Instance ID'),
+                IconColumn::make('is_active')->boolean()->label('Status'),
+                TextColumn::make('created_at')
                     ->dateTime()
                     ->sortable()
                     ->toggleable(isToggledHiddenByDefault: true),
-                Tables\Columns\TextColumn::make('updated_at')
+                TextColumn::make('updated_at')
                     ->dateTime()
                     ->sortable()
                     ->toggleable(isToggledHiddenByDefault: true),
@@ -53,7 +59,24 @@ class WhatsAppServerResource extends Resource
                 //
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
+                ActionGroup::make([
+                    Action::make('connect')
+                        ->label(fn($record) => match ($record->is_active) {
+                            0 => 'Scan QR',
+                            1 => 'Putuskan',
+                            default => null,
+                        })
+                        ->url(fn($record) => match ($record->is_active) {
+                            default => null,
+                        })
+                        ->icon(fn($record) => match ($record->is_active) {
+                            0 => 'heroicon-o-qr-code',
+                            1 => 'heroicon-o-x-circle',
+                            default => null,
+                        }),
+                    EditAction::make(),
+                    DeleteAction::make(),
+                ])
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
@@ -64,11 +87,8 @@ class WhatsAppServerResource extends Resource
 
     public static function getRelations(): array
     {
-        return [
-            //
-        ];
+        return [];
     }
-
     public static function getPages(): array
     {
         return [
