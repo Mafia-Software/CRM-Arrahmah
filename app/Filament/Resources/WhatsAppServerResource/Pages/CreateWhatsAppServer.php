dc2d05b0d64122e9abba6cee1a635d801b887c87
Nafi'ul Anam <anamnafiul99@gmail.com>
2024-12-25 21:56:09 +0700

    redirect to index and custom create logic on WhatsappServer

--- app/Filament/Resources/WhatsAppServerResource/Pages/CreateWhatsAppServer.php
+++ app/Filament/Resources/WhatsAppServerResource/Pages/CreateWhatsAppServer.php
@@ -3,10 +3,31 @@
 namespace App\Filament\Resources\WhatsAppServerResource\Pages;
 
 use App\Filament\Resources\WhatsAppServerResource;
+use App\Http\Controllers\API\WhatsappController;
+use App\Services\WhatsappService;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Database\Eloquent\Model;
+
 
 class CreateWhatsAppServer extends CreateRecord
 {
     protected static string $resource = WhatsAppServerResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
+    protected function handleRecordCreation(array $data): Model
+    {
+        $wa = new WhatsappController(new WhatsappService);
+        $response = $wa->createInstance();
+        if ($response['instance_id'] == null) {
+            $this->cancel();
+        }
+        $data['instance_id'] = $response['instance_id'];
+
+        return static::getModel()::create($data);
+    }
 }
