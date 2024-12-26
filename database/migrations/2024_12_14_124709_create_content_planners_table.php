ac400b8c26d41b364716938b4396a82cbe98727c
Irfan Maulana Khakiki <irfankhakiki17@gmail.com>
2024-12-26 14:15:34 +0700

    add tanggal kolom

--- database/migrations/2024_12_14_124709_create_content_planners_table.php
+++ database/migrations/2024_12_14_124709_create_content_planners_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('content_planners', function (Blueprint $table) {
             $table->id();
             $table->string("pesan");
+            $table->date('tanggal');
             $table->timestamps();
         });
     }
