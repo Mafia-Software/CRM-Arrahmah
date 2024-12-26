739f9670e5e3c656569bc7c567e5b3db5514bcae
Irfan Maulana Khakiki <irfankhakiki17@gmail.com>
2024-12-26 14:13:25 +0700

    redirect to index after create

--- app/Filament/Resources/ResponseResource/Pages/CreateResponse.php
+++ app/Filament/Resources/ResponseResource/Pages/CreateResponse.php
@@ -8,4 +8,9 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateResponse extends CreateRecord
 {
     protected static string $resource = ResponseResource::class;
+
+     protected function getRedirectUrl(): string
+    {
+        return static::getResource()::getUrl('index');
+    }
 }
--- app/Filament/Resources/WhatsAppServerResource/Pages/CreateWhatsAppServer.php
+++ app/Filament/Resources/WhatsAppServerResource/Pages/CreateWhatsAppServer.php
@@ -9,4 +9,9 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateWhatsAppServer extends CreateRecord
 {
     protected static string $resource = WhatsAppServerResource::class;
+
+     protected function getRedirectUrl(): string
+    {
+        return static::getResource()::getUrl('index');
+    }
 }
