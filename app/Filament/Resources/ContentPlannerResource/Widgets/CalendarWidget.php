fa17a4343d0683faaf17d76d5eea76dc89432c52
Nafi'ul Anam <anamnafiul99@gmail.com>
2024-12-27 13:36:41 +0700

    create CalendarWidget for ContentPlanner

--- app/Filament/Resources/ContentPlannerResource/Widgets/CalendarWidget.php
+++ app/Filament/Resources/ContentPlannerResource/Widgets/CalendarWidget.php
@@ -2,29 +2,61 @@
 
 namespace App\Filament\Resources\ContentPlannerResource\Widgets;
 
-use App\Filament\Resources\ContentPlannerResource;
 use App\Models\ContentPlanner;
-use Filament\Widgets\Widget;
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Illuminate\Database\Eloquent\Model;
 use Saade\FilamentFullCalendar\Widgets\FullCalendarWidget;
+use Saade\FilamentFullCalendar\Actions\CreateAction;
+use Saade\FilamentFullCalendar\Actions\DeleteAction;
 
 class CalendarWidget extends FullCalendarWidget
 {
-    // protected static string $view = 'filament.resources.content-planner-resource.widgets.calendar-widget';
+    public Model | string | null $model = ContentPlanner::class;
     public function fetchEvents(array $fetchInfo): array
     {
         return ContentPlanner::query()
-            ->where('starts_at', '>=', $fetchInfo['start'])
-            ->where('ends_at', '<=', $fetchInfo['end'])
+            ->where('tanggal', '>=', $fetchInfo['start'])
             ->get()
             ->map(
-                fn (ContentPlanner $event) => [
-                    'title' => $event->id,
-                    'start' => $event->starts_at,
-                    'end' => $event->ends_at,
-                    'url' => ContentPlannerPage::getUrl(name: 'index', parameters: ['record' => $event]),
-                    'shouldOpenUrlInNewTab' => true
+                fn(ContentPlanner $event) => [
+                    'id' => $event->id,
+                    'title' => $event->pesan,
+                    'start' => $event->tanggal,
                 ]
             )
             ->all();
+    }
+
+    protected function modalActions(): array
+    {
+        return [
+            CreateAction::make()
+                ->mountUsing(
+                    function (Form $form, array $arguments) {
+                        $form->fill([
+                            'tanggal' => $arguments['start'] ?? null,
+                        ]);
+                    }
+                )->createAnother(false),
+            DeleteAction::make(),
+        ];
+    }
+    public function getFormSchema(): array
+    {
+        return [
+            // RichEditor::make('pesan')->required()->toolbarButtons([
+            //     'bold',
+            //     'italic',
+            //     'underline',
+            // ]),
+            TextInput::make('pesan')->required(),
+            DatePicker::make('tanggal')->required()->disabled(),
+        ];
+    }
+    protected function headerActions(): array
+    {
+        return [];
     }
 }
