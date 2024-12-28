bdd970eb264e5ed451ba2680e861b2977f092fc8
Nafi'ul Anam <anamnafiul99@gmail.com>
2024-12-28 07:03:16 +0700

    change migration

--- database/migrations/2024_12_21_021140_create_whatsapp_servers_table.php
+++ database/migrations/2024_12_21_021140_create_whatsapp_servers_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     /**
      * Run the migrations.
      */
@@ -15,6 +14,7 @@ return new class extends Migration
             $table->id();
             $table->string('nama');
             $table->string('no_wa');
+            $table->string('apiKey');
             $table->integer('delay');
             $table->boolean('is_active')->default(false);
             $table->timestamps();
