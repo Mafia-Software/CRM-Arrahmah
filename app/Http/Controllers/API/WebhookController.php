91de4fc710fb3e0540a4d50545177c802a3cba6b
Nafi'ul Anam <anamnafiul99@gmail.com>
2024-12-28 10:20:20 +0700

    update webhook handler

--- app/Http/Controllers/API/WebhookController.php
+++ app/Http/Controllers/API/WebhookController.php
@@ -3,11 +3,27 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Log;
+use App\Models\WhatsappServer;
+use Illuminate\Http\Request;
+
 
 class WebhookController extends Controller
 {
-    public function index()
+    public function handle(Request $request)
     {
-        return 'OK';
+        Log::create([
+            'type' => 'webhook',
+            'logs' => request()->all()
+        ]);
+        switch ($request['type']) {
+            case 'change_state':
+                WhatsappServer::where('api_key', $request['results']['apiKey'])
+                    ->update(['service_status' => $request['results']['state']]);
+                break;
+
+            default:
+                break;
+        }
     }
 }
