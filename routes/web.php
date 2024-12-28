503816f3dbf35cd1f2eff7c62f3242d3689358e3
Nafi'ul Anam <anamnafiul99@gmail.com>
2024-12-28 08:15:45 +0700

    remove web.php

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,1 @@
 <?php
-
-use App\Services\WhatsappService;
-use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\Route;
-use Symfony\Component\HttpFoundation\Request;
-use App\Http\Controllers\API\WhatsappController;
-use App\Http\Controllers\WhatsAppServerController;
-
-Route::get('/', function () {
-    return redirect('/admin');
-});
-
-Route::post('/sendMessage', [WhatsappController::class, 'send']);
-
-
-Route::post('/create-wa-server', [WhatsAppServerController::class, 'createwaserver']);
