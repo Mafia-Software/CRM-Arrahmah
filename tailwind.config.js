db95a8dd16b37fc8e3e77e39e5eb0adfae4f5854
Nafi'ul Anam <anamnafiul99@gmail.com>
2024-12-29 13:54:35 +0700

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,7 +17,7 @@ module.exports = {
 };
 
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
     content: [
         "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
         "./storage/framework/views/*.php",
