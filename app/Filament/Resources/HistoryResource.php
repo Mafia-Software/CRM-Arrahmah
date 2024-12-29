959666c3c924032594fa8e0155e17257dac3e2de
Nafi'ul Anam <anamnafiul99@gmail.com>
2024-12-29 11:24:49 +0700

    update historyresource

--- app/Filament/Resources/HistoryResource.php
+++ app/Filament/Resources/HistoryResource.php
@@ -5,10 +5,17 @@ namespace App\Filament\Resources;
 use App\Filament\Resources\HistoryResource\Pages;
 use App\Filament\Resources\HistoryResource\RelationManagers;
 use App\Models\History;
+use Faker\Provider\ar_EG\Text;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Actions\Action;
+use Filament\Tables\Actions\BulkActionGroup;
+use Filament\Tables\Actions\DeleteAction;
+use Filament\Tables\Actions\DeleteBulkAction;
+use Filament\Tables\Actions\ViewAction;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -25,44 +32,41 @@ class HistoryResource extends Resource
 
     public static function form(Form $form): Form
     {
-        return $form
-            ->schema([
-                //
-            ]);
+        return $form;
     }
 
     public static function table(Table $table): Table
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('id')->label('ID'),
+                TextColumn::make('user.name')->label('Admin'),
+                TextColumn::make('whatsappServer.nama')->label('WA Server'),
+                TextColumn::make('contentPlanner.pesan')->label('Pesan'),
+                TextColumn::make('created_at')->label('Tanggal'),
             ])
             ->filters([
                 //
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
+                DeleteAction::make(),
             ])
             ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
                 ]),
             ])->emptyStateHeading('Belum Ada Riwayat');
     }
 
     public static function getRelations(): array
     {
-        return [
-            //
-        ];
+        return [];
     }
 
     public static function getPages(): array
     {
         return [
             'index' => Pages\ListHistories::route('/'),
-            'create' => Pages\CreateHistory::route('/create'),
-            'edit' => Pages\EditHistory::route('/{record}/edit'),
         ];
     }
 }
