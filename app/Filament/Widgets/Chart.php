d8916c35d3a2a4b88183ad282963013695e43778
Nafi'ul Anam <anamnafiul99@gmail.com>
2024-12-29 13:54:08 +0700

    add dynamic data to dashboard widget

--- app/Filament/Widgets/Chart.php
+++ app/Filament/Widgets/Chart.php
@@ -2,20 +2,30 @@
 
 namespace App\Filament\Widgets;
 
+use App\Models\PesanKeluar;
 use Filament\Widgets\ChartWidget;
+use Illuminate\Support\Carbon;
 
 class Chart extends ChartWidget
 {
-    protected static ?string $heading = 'Chart';
+    protected static ?string $heading = 'Statistik Pesan Keluar';
     protected static ?int $sort = 20;
     protected int | string | array $columnSpan = 'full';
     protected function getData(): array
     {
+        $data = [];
+        for ($month = 1; $month <= 12; $month++) {
+            $startDate = Carbon::create(date('Y'), $month, 1);
+            $endDate = $startDate->copy()->endOfMonth();
+
+            $count = PesanKeluar::whereBetween('created_at', [$startDate, $endDate])->count();
+            $data[$month - 1] = $count;
+        }
         return [
             'datasets' => [
                 [
-                    'label' => 'Blog posts created',
-                    'data' => [0, 10, 5, 2, 21, 32, 45, 74, 65, 45, 77, 89],
+                    'label' => 'Statistik Pesan Keluar',
+                    'data' => $data,
                 ],
             ],
             'labels' => ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -2,18 +2,27 @@
 
 namespace App\Filament\Widgets;
 
+use App\Models\PesanKeluar;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 
 class StatsOverview extends BaseWidget
 {
+    /**
+     * Get the stats for the widget.
+     *
+     * @return array
+     */
     protected function getStats(): array
     {
+        $totalFeedback = 500;
+        $totalTerbaca = PesanKeluar::where('status', 4)->count();
+        $totalTerkirim = PesanKeluar::whereIn('status', [2, 200])->count();
+
         return [
-            Stat::make('Feedback', '500'),
-            Stat::make('Terbaca', '2000'),
-            Stat::make('Terkirim', '3000'),
-            Stat::make('Tidak Menemukan Wangsaff', '1000'),
+            Stat::make('Feedback', $totalFeedback),
+            Stat::make('Terbaca', $totalTerbaca),
+            Stat::make('Terkirim', $totalTerkirim),
         ];
     }
 }
