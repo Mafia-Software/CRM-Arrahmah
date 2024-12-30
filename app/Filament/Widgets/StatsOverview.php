3552204e1b4f48cd3f15619a37623ad81a05b0cf
Nafi'ul Anam <anamnafiul99@gmail.com>
2024-12-31 03:38:51 +0700

    update totalFeedback in statsOverview Widget

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Widgets;
 
 use App\Models\PesanKeluar;
+use App\Models\PesanMasuk;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 
@@ -15,7 +16,7 @@ class StatsOverview extends BaseWidget
      */
     protected function getStats(): array
     {
-        $totalFeedback = 500;
+        $totalFeedback = PesanMasuk::count();
         $totalTerbaca = PesanKeluar::where('status', 4)->count();
         $totalTerkirim = PesanKeluar::whereIn('status', [2, 200])->count();
 
