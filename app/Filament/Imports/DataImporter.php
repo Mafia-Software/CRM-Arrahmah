25c75db73dce767f8b551c826291194d551cf48b
Irfan Maulana Khakiki <irfankhakiki17@gmail.com>
2024-12-31 16:09:29 +0700

    fix upload data

--- app/Filament/Imports/DataImporter.php
+++ app/Filament/Imports/DataImporter.php
@@ -3,8 +3,9 @@
 namespace App\Filament\Imports;
 
 use App\Models\Data;
-use Filament\Actions\Imports\ImportColumn;
+use App\Models\UnitKerja;
 use Filament\Actions\Imports\Importer;
+use Filament\Actions\Imports\ImportColumn;
 use Filament\Actions\Imports\Models\Import;
 
 class DataImporter extends Importer
@@ -15,6 +16,46 @@ class DataImporter extends Importer
     {
         return [
             //
+            ImportColumn::make('Nama')
+                ->requiredMapping()
+                ->label('NAMA')
+                ->rules(['required']),
+            ImportColumn::make('No Wa')
+                ->requiredMapping()
+                ->label('No Whatsapp')
+                ->rules(['required'])
+                ->castStateUsing(function (string $state): ?string {
+                    if (blank($state)) {
+                        return null;
+                    }
+
+                    // Memeriksa apakah nomor diawali dengan '62'
+                    if (substr($state, 0, 2) === '62') {
+                        // Mengubah awalan '62' menjadi '0'
+                        return '0' . substr($state, 2);
+                    }
+
+                    // Jika tidak dimulai dengan '62', kembalikan nomor seperti semula
+                    return $state;
+                }),
+            ImportColumn::make('UNIT OWNER')
+                ->requiredMapping()
+                ->label('Unit Kerja')
+                ->rules(['required'])
+                ->relationship(
+                    resolveUsing: function (string $state) {
+                        // Mencari Unit Kerja di database berdasarkan nama unit kerja dari file Excel
+                        $unitKerja = UnitKerja::where('name', $state)->first();
+
+                        if ($unitKerja) {
+                            // Jika unit kerja ditemukan, kembalikan objek unit kerja
+                            return $unitKerja;
+                        }
+
+                        // Jika unit kerja tidak ditemukan, kembalikan pesan "unit kerja tidak ditemukan"
+                        return $state; // Kembalikan nilai string nama unit kerja yang tidak ditemukan
+                    },
+                ),
         ];
     }
 
