c7bea5da19bbdedf277c67ee548d3d63a8920722
Nafi'ul Anam <anamnafiul99@gmail.com>
2025-01-01 10:51:23 +0700

    update migration

--- database/migrations/2024_12_30_040302_create_imports_table.php
+++ database/migrations/2024_12_30_040302_create_imports_table.php
@@ -4,7 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
@@ -12,11 +13,14 @@ return new class extends Migration {
     {
         Schema::create('imports', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->timestamp('completed_at')->nullable();
             $table->string('file_name');
             $table->string('file_path');
             $table->string('importer');
-            $table->integer('total_rows');
+            $table->unsignedInteger('processed_rows')->default(0);
+            $table->unsignedInteger('total_rows');
+            $table->unsignedInteger('successful_rows')->default(0);
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
     }
