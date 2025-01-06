67366e9a34c05d103625b1b6666cc774d5a69e01
Nafi'ul Anam <anamnafiul99@gmail.com>
2025-01-06 14:21:22 +0700

    limit pesan character in histories

--- app/Filament/Resources/HistoryResource.php
+++ app/Filament/Resources/HistoryResource.php
@@ -41,7 +41,7 @@ class HistoryResource extends Resource
                 ProgressColumn::make('progress')->label('Progress')->color('bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500')->progress(function ($record) {
                     return Bus::findBatch($record->batch_id)->progress();
                 }),
-                TextColumn::make('contentPlanner.pesan')->label('Pesan'),
+                TextColumn::make('contentPlanner.pesan')->label('Pesan')->limit(100),
                 TextColumn::make('created_at')->label('Tanggal'),
             ])->defaultSort('created_at', 'desc')
             ->filters([
