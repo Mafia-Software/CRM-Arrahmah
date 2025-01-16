5762d60534490a402dd0be574c3cb00253b749d3
Nafi'ul Anam <anamnafiul99@gmail.com>
2025-01-16 21:52:42 +0700

    add scheduler for queue work

--- routes/console.php
+++ routes/console.php
@@ -1,18 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\DB;
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
-use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Support\Facades\Schedule;
 
-// $delaySchedules = DB::table('whatsapp_servers')->get();
-
-// app(Schedule::class)->tap(function (Schedule $schedule) use ($delaySchedules) {
-//     foreach ($delaySchedules as $delay) {
-//         $delayInMinutes = ceil($delay->delay / 60); // Konversi ke menit jika delay dalam detik
-//         $schedule->command("queue:work --stop-when-empty")
-//             ->everyMinutes($delayInMinutes) // Gunakan delay dari kolom
-//             ->withoutOverlapping()
-//             ->description("Queue Worker for Server {$delay->name}");
-//     }
-// });
+Schedule::command('queue:work --stop-when-empty')->everyMinute()->withoutOverlapping();
