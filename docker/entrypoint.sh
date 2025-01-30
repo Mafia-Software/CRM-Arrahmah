b1641516759fd3cf9094d1cd74c4d5a6bd35bc77
Nafi'ul Anam <anamnafiul99@gmail.com>
2025-01-30 21:49:29 +0700

    update docker entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-
 cd /home/crm
 
 # php artisan migrate:fresh --seed
 php artisan key:generate
 php artisan optimize
 php artisan filament:optimize
+php artisan storage:link
 
 /usr/bin/supervisord -c /etc/supervisord.conf
