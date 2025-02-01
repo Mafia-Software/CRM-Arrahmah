cb60f97d0004d67b9861fa9bd90db6c6300fe8ea
Nafi'ul Anam <anamnafiul99@gmail.com>
2025-02-01 18:58:04 +0700

    update logout link

--- resources/views/components/sidebarend.blade.php
+++ resources/views/components/sidebarend.blade.php
@@ -5,7 +5,7 @@
     </div>
     <span class="font-medium text-white">{{ Auth::user()->name ?? 'ADMIN' }}</span>
 
-    <form action="/admin/logout" method="POST">
+    <form action="/logout" method="POST">
         @csrf
         <button type="submit"
             class="mt-2 flex items-center rounded-lg bg-white px-4 py-2 text-sm font-semibold text-purple-700">
